d741a9b2f22706b93193f81359e5b65fa54e7f5b
sesiliarichard <sesiliarichard2003@gmail.com>
2026-04-01 11:38:30 +0300

    added website files

--- profile.js
+++ profile.js
@@ -377,6 +377,7 @@ function formatMemberSince(dateString) {
     if (!dateString) return 'Unknown';
     return new Date(dateString).toLocaleDateString('en-US', {
         month: 'long',
+        day:   'numeric',
         year:  'numeric'
     });
 }
